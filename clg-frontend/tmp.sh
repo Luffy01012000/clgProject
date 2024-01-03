40ad7a370425a931c696fb453fed10a1ede72ab0
Goku <goku05555@gmail.com>
2024-01-03 10:25:31 +0530

    again fixed dockerfile for jenkins build issue

--- clg-frontend/tmp.sh
+++ clg-frontend/tmp.sh
@@ -1,12 +1,9 @@
-img=( "backend-deployment-65b44cb859",
-"backend-deployment-68b6dddb89",
-"backend-deployment-6b8f596565",
- "metrics-server-fbb469ccc"
+img=(
 )
 
 for i in ${img[@]};do
-#  docker rmi $i
- kubectl delete replicaset  $i
-# echo $i
+ docker rmi $i
+#  kubectl delete replicaset  $i
+echo $i
  done
 
