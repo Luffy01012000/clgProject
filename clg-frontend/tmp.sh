010d161fe754cd0ec47b63c8f667c538a4cda15e
Goku <goku05555@gmail.com>
2024-01-03 10:34:25 +0530

    Revert "again fixed dockerfile for jenkins build issue"
    This reverts commit 40ad7a370425a931c696fb453fed10a1ede72ab0.

--- clg-frontend/tmp.sh
+++ clg-frontend/tmp.sh
@@ -1,9 +1,12 @@
-img=(
+img=( "backend-deployment-65b44cb859",
+"backend-deployment-68b6dddb89",
+"backend-deployment-6b8f596565",
+ "metrics-server-fbb469ccc"
 )
 
 for i in ${img[@]};do
- docker rmi $i
-#  kubectl delete replicaset  $i
-echo $i
+#  docker rmi $i
+ kubectl delete replicaset  $i
+# echo $i
  done
 
